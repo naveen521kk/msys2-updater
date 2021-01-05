7799228c13c3cdc15a334a2aefe7ff9176784ac9
Syrus Dark <naveen@syrusdark.website>
2021-01-05 14:10:58 +0530

    logging with colors

--- updator/__init__.py
+++ updator/__init__.py
@@ -1,22 +1,23 @@
 import argparse
 import json
-import logging
+from .logger import logger,console
 from pathlib import Path
 from updator.writer import Writer
-
+import logging
 from .constants import PACKAGES_PATH
 from .handlers.github import GithubHandler
 from .handlers.gitlab import GitlabHandler
 from .handlers.pypi import PyPiHandler
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true", help="enable debug mode")
     args = parser.parse_args()
 
     if args.debug:
-        logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.DEBUG)
+        logging.basicConfig(filename='update.log',format="%(levelname)s - %(message)s", level=logging.DEBUG)
     else:
-        logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.INFO)
+        logging.basicConfig(filename='update.log',format="%(levelname)s - %(message)s", level=logging.INFO)
     for file in Path(PACKAGES_PATH).glob("*.json"):
         # with open(file) as f:
         #     info = json.load(f)
@@ -32,5 +33,10 @@ def main():
         with open(file) as f:
             info = json.load(f)
         if info["type"] == "pypi":
-            a = PyPiHandler(info)
-            Writer(info,a)
+            try:
+                logger.info("Checking %s",info['name'])
+                a = PyPiHandler(info)
+                Writer(info, a)
+            except Exception as e:
+                console.print_exception()
+                logging.error(e)
--- updator/logger.py
+++ updator/logger.py
@@ -1,3 +1,26 @@
 import logging
+from rich.console import Console
+from rich.logging import RichHandler
+from rich.theme import Theme
 
-logger = logging.getLogger('msys2-updator')
+logger = logging.getLogger("msys2-updator")
+
+custom_theme = Theme(
+    {
+        "log.level": "magenta",
+        "repr.number": "bold green blink",
+        "log.message": "white on red",
+        "logging.level.info": "green on white",
+        "log.time":"green dim",
+        "logging.level.notset":"red on white"
+    },
+)
+console=Console(theme=custom_theme)
+logger.addHandler(
+    RichHandler(
+        show_time=False,
+        show_path=False,
+        console=console,
+        rich_tracebacks=True
+    )
+)
