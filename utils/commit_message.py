29375d896a7875f5190e487001ff6135a3b56056
Syrus Dark <naveen@syrusdark.website>
2021-01-11 20:34:06 +0530

    Dynamically create commit message

---
+++ utils/commit_message.py
@@ -0,0 +1,14 @@
+import json
+
+from pathlib import Path
+
+PACKAGES_PATH = Path(__file__).parent.resolve().parent / "packages" 
+commit_message = []
+for file in Path(PACKAGES_PATH).glob("*.json"):
+    with open(file) as f:
+        info = json.load(f)
+    if "update" in info:
+        if info["update"]:
+            commit_message.append(info["name"])
+
+print(f"::set-output name=commit_message::{' '.join(commit_message)}")
