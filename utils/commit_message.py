7325bb177045400a1d83fde5f4186ae5efccc51d
Syrus Dark <naveen@syrusdark.website>
2021-01-20 18:06:33 +0530

    use sys.path manipulation correctly

--- utils/commit_message.py
+++ utils/commit_message.py
@@ -1,31 +1,36 @@
 import sys
-sys.path.append("..")
-
-import json
 from pathlib import Path
-from git import Repo
-from git import Actor
+
+sys.path.append(Path(__file__).parent.resolve().parent)
+import json
 import os
 
+from git import Actor, Repo
 from updator.utils import get_repo_path
-committer_email = os.getenv("AUTHOR_EMAIL","41898282+github-actions[bot]@users.noreply.github.com")
-PACKAGES_PATH = Path(__file__).parent.resolve().parent / "packages" 
+
+committer_email = os.getenv(
+    "AUTHOR_EMAIL", "41898282+github-actions[bot]@users.noreply.github.com"
+)
+PACKAGES_PATH = Path(__file__).parent.resolve().parent / "packages"
 commit_message = []
 for file in Path(PACKAGES_PATH).glob("*.json"):
     with open(file) as f:
         info = json.load(f)
     if "update" in info:
         if info["update"]:
-            repo_path = get_repo_path()
+            repo_path = get_repo_path(info)
             repo = Repo(repo_path)
             index = repo.index
             index.add(f'{info["name"]}/"PKGBUILD"')
-            name = info["name"].replace("mingw-w64-","")
+            name = info["name"].replace("mingw-w64-", "")
             commit_message = f'{name}:update to {info["version"]}'
-            author = Actor("github-actions[bot]", "41898282+github-actions[bot]@users.noreply.github.com")
+            author = Actor(
+                "github-actions[bot]",
+                "41898282+github-actions[bot]@users.noreply.github.com",
+            )
             committer = Actor("Naveen", committer_email)
             index.commit(commit_message, author=author, committer=committer)
 
-#message = "\n- " + "\n- ".join(commit_message)
-#message = " ".join(commit_message)
-#print(f"UPDATE {len(commit_message)}:{message}")
+# message = "\n- " + "\n- ".join(commit_message)
+# message = " ".join(commit_message)
+# print(f"UPDATE {len(commit_message)}:{message}")
