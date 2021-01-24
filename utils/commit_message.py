bb2ddb1eb82bf79aa528cec9e033e4d08e2a6834
Syrus Dark <naveen@syrusdark.website>
2021-01-25 01:27:06 +0530

    use try/except

--- utils/commit_message.py
+++ utils/commit_message.py
@@ -14,23 +14,26 @@ committer_email = os.getenv(
 PACKAGES_PATH = Path(__file__).parent.resolve().parent / "packages"
 commit_message = []
 for file in Path(PACKAGES_PATH).glob("*.json"):
-    with open(file) as f:
-        info = json.load(f)
-    if "update" in info:
-        if info["update"]:
-            repo_path = get_repo_path(info)
-            repo = Repo(repo_path)
-            index = repo.index
-            repo.git.add(f'{info["name"]}/PKGBUILD')
-            name = info["name"].replace("mingw-w64-", "")
-            commit_message = f'{name}: update to {info["version"]}'
-            author = Actor(
-                "github-actions[bot]",
-                "41898282+github-actions[bot]@users.noreply.github.com",
-            )
-            committer = Actor("Naveen", committer_email)
-            index.commit(commit_message, author=author, committer=committer)
-            print(commit_message)
+    try:
+        with open(file) as f:
+            info = json.load(f)
+        if "update" in info:
+            if info["update"]:
+                repo_path = get_repo_path(info)
+                repo = Repo(repo_path)
+                index = repo.index
+                repo.git.add(f'{info["name"]}/PKGBUILD')
+                name = info["name"].replace("mingw-w64-", "")
+                commit_message = f'{name}: update to {info["version"]}'
+                author = Actor(
+                    "github-actions[bot]",
+                    "41898282+github-actions[bot]@users.noreply.github.com",
+                )
+                committer = Actor("Naveen", committer_email)
+                index.commit(commit_message, author=author, committer=committer)
+                print(commit_message)
+    except KeyError as e:
+        print(e)
 
 # message = "\n- " + "\n- ".join(commit_message)
 # message = " ".join(commit_message)
