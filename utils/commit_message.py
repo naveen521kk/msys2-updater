9b6797c730947552246fdec9602d2fea06aa855f
Naveen M K <naveen@syrusdark.website>
2021-03-06 13:58:35 +0530

    use github actions as commitor

--- utils/commit_message.py
+++ utils/commit_message.py
@@ -26,8 +26,12 @@ for file in Path(PACKAGES_PATH).glob("*.json"):
                 name = info["name"].replace("mingw-w64-", "")
                 print(name)
                 commit_message = f'{name}: update to {info["version"]}'
+                committer = Actor(
+                    "github-actions[bot]",
+                    "41898282+github-actions[bot]@users.noreply.github.com",
+                )
                 author = Actor("Naveen", committer_email)
-                index.commit(commit_message, author=author)
+                index.commit(commit_message, author=author, committer=committer)
                 print(commit_message)
     except KeyError as e:
         print(e)
