naveen521kk/msys2-updater

commit 136911fdb8ed7ed369b6e5dbdf0a6cc91fd123a9

Sort version before picking the recent version from remote Essentially I picked the last returned version from Github or Gitlab API as the recent one but that isn't true, because there could minor version released to previous major version, see glib's or gtk's or meson's tags for example, and it isn't the latest in that case. So, add a version comparing class to sort, and the picked the latest one.
Currently, it inherits a class from disutils which is depreciated
and could be removed soon, and it is removed I should change it,
but is should be fine currently.

Signed-off-by: Naveen M K <naveen@syrusdark.website>

updator/handlers/github.py
@@ -5,6 +5,7 @@ import os
 from github import Github
 
 from .handler import Handler
+from ..utils import VersionSort
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", None)
 
@@ -19,12 +20,13 @@ class GithubHandler(Handler):
 
     @property
     def remote_version(self) -> None:
-        if hasattr(self,'_remote_version'):
+        if hasattr(self, "_remote_version"):
             return self._remote_version
         gh = self.gh
         info = self.info
         repo = gh.get_repo(info["slug"])
-        latest = repo.get_tags()[0]
-        version = latest.name if latest.name[0]!="v" else latest.name[1:]
+        versions = repo.get_tags()
+        latest = versions.sort(key=VersionSort)[-1]
+        version = latest.name if latest.name[0] != "v" else latest.name[1:]
         self._remote_version = version
         return version

updator/handlers/gitlab.py
@@ -5,6 +5,7 @@ import os
 from gitlab import Gitlab
 
 from .handler import Handler
+from ..utils import VersionSort
 
 GITLAB_TOKEN = os.getenv("GITLAB_TOKEN", None)
 
@@ -19,12 +20,13 @@ class GitlabHandler(Handler):
 
     @property
     def remote_version(self) -> None:
-        if hasattr(self,'_remote_version'):
+        if hasattr(self, "_remote_version"):
             return self._remote_version
         gl = self.gl
         info = self.info
         repo = gl.projects.get(info["id"])
-        latest = repo.tags.list()[0]
-        version = latest.name if latest.name[0]!="v" else latest.name[1:]
+        versions = repo.get_tags()
+        latest = versions.sort(key=VersionSort)[-1]
+        version = latest.name if latest.name[0] != "v" else latest.name[1:]
         self._remote_version = version
         return version

updator/utils.py
@@ -9,6 +9,7 @@ import shlex
 import tempfile
 import textwrap
 from pathlib import Path
+from distutils.version import Version
 
 
 def vercmp(v1: str, v2: str) -> int:
@@ -123,6 +124,47 @@ def vercmp(v1: str, v2: str) -> int:
 
 def version_is_newer_than(v1: str, v2: str) -> bool:
     return vercmp(v1, v2) == 1
+
+
+class VersionSort(Version):
+    """VersionSort, compare two version using ``>`` and ``<``
+    using the previously defined version comparing functions.
+
+    This inherits from ``distutils.version.Version``, when it is
+    depreciated, compatibity code should be added.
+    This uses `version_is_newer_than` to check whether the version
+    passed here is newer than the other, to which comparing to.
+    This is helpful when using ``list.sort(key=VersionSort)``.
+
+    Examples
+    ========
+
+    >>> a = VersionSort("1.0.0")
+    >>> b = VersionSort("2.0.0")
+    >>> a > b
+    False
+    >>> a < b
+    True
+    >>> c = ['1.0.0','1.1.0','1.0.1','1.0.2','1.0.3','1.1.3','1.1.2']
+    >>> c.sort(key=VersionSort)
+    >>> c
+    ['1.0.0', '1.0.1', '1.0.2', '1.0.3', '1.1.0', '1.1.2', '1.1.3']
+    """
+
+    def parse(self, vstring):
+        self.version = vstring
+
+    def __str__(self):
+        return self.versions
+
+    def _cmp(self, other):
+        if isinstance(other, str):
+            other = VersionSort(other)
+        elif not isinstance(other, VersionSort):
+            return NotImplemented
+        if version_is_newer_than(self.version, other.version):
+            return 1
+        return -1
 
 
 def find_checksum_from_file(fname, hashtype, info):

commit bf7f98d8cbecb2e12bd6aeda3a0088d4e0c94d2d

Fix Github and Gitlab Handlers 136911fdb8ed7ed369b6e5dbdf0a6cc91fd123a9 introduced sorting version before picking the latest and it was broken for both github and gitlab handlers this fixes that one.

updator/handlers/github.py
@@ -25,8 +25,9 @@ class GithubHandler(Handler):
         gh = self.gh
         info = self.info
         repo = gh.get_repo(info["slug"])
-        versions = repo.get_tags()
-        latest = versions.sort(key=VersionSort)[-1]
-        version = latest.name if latest.name[0] != "v" else latest.name[1:]
+        versions = repo.get_tags()[:20]
+        version_list = [v.name if v.name[0] != "v" else v.name[1:] for v in versions]
+        version_list.sort(key=VersionSort)
+        version = version_list[-1]
         self._remote_version = version
         return version

updator/handlers/gitlab.py
@@ -25,8 +25,11 @@ class GitlabHandler(Handler):
         gl = self.gl
         info = self.info
         repo = gl.projects.get(info["id"])
-        versions = repo.get_tags()
-        latest = versions.sort(key=VersionSort)[-1]
-        version = latest.name if latest.name[0] != "v" else latest.name[1:]
+        versions = repo.tags.list(page=1, per_page=20)
+        version_list = [
+            v.name if v.name[0] != "v" else v.name[1:] for v in versions
+        ]
+        version_list.sort(key=VersionSort)
+        version = version_list[-1]
         self._remote_version = version
         return version