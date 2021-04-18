17be82302d56e84c0771480636f89b9f15e4aa30
Naveen M K <naveen@syrusdark.website>
2021-04-18 23:03:24 +0530

    update version_parse regex it misses alphabets which are allowed

--- updator/constants.py
+++ updator/constants.py
@@ -12,7 +12,7 @@ class Regex(Enum):
     checksum: re.Pattern = re.compile(
         r"(?P<type>\w*)sums=\((?P<checksum>[^\(\)]*)\)\n", re.MULTILINE
     )
-    version_parse: re.Pattern = re.compile(r"pkgver=(?P<version>[\d\.\"\"]*)")
+    version_parse: re.Pattern = re.compile(r"pkgver=(?P<version>([\w\d\.\'\"])*)")
     pkgrel_parse: re.Pattern = re.compile(r"pkgrel=(?P<version>[\d\.\"\"]*)")
     dependency = re.compile(r"^depends=\((?P<checksum>[^\(\)]*)\)\n", re.MULTILINE)
     # from https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
