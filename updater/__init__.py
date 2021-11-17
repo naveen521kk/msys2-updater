1868595d459f50389b8c03ff21bafc7765cbc92e
Naveen M K <naveen521kk@gmail.com>
2021-11-17 17:16:42 +0530

    Don't set logging level to debug always

--- updater/__init__.py
+++ updater/__init__.py
@@ -14,42 +14,53 @@ from .writer import Writer
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", action="store_true", help="enable debug mode",)
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="enable debug mode",
+    )
     args = parser.parse_args()
 
-
-    logging.basicConfig(filename='update.log',format="%(levelname)s - %(message)s", level=logging.DEBUG)
+    if args.debug:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+    logging.basicConfig(
+        filename="update.log",
+        format="%(levelname)s - %(message)s",
+        level=logging_level,
+    )
     for file in Path(PACKAGES_PATH).glob("*.json"):
         with open(file) as f:
             info = json.load(f)
         try:
             if info["type"] == "pypi":
-                logger.info("Checking %s method PyPi",info['name'])
+                logger.info("Checking %s method PyPi", info["name"])
                 a = PyPiHandler(info)
                 info["version"] = a.remote_version
-                with open(file,'w') as f:
-                    json.dump(info,f,indent=4)
+                with open(file, "w") as f:
+                    json.dump(info, f, indent=4)
                 Writer(info, a)
                 with open(file) as f:
                     info = json.load(f)
                 # check for dependency changes now
-                PyPiDepsManager(a,info)
+                PyPiDepsManager(a, info)
             elif info["type"] == "github":
-                logger.info("Checking %s method Github",info['name'])
+                logger.info("Checking %s method Github", info["name"])
                 a = GithubHandler(info)
                 info["version"] = a.remote_version
-                with open(file,'w') as f:
-                    json.dump(info,f,indent=4)
-                Writer(info,a)
+                with open(file, "w") as f:
+                    json.dump(info, f, indent=4)
+                Writer(info, a)
             elif info["type"] == "gitlab":
-                logger.info("Checking %s method Gitlab",info['name'])
+                logger.info("Checking %s method Gitlab", info["name"])
                 a = GitlabHandler(info)
                 info["version"] = a.remote_version
-                with open(file,'w') as f:
-                    json.dump(info,f,indent=4)
-                Writer(info,a)
+                with open(file, "w") as f:
+                    json.dump(info, f, indent=4)
+                Writer(info, a)
             else:
-                logger.error("Unknown handle %s",info["type"])
+                logger.error("Unknown handle %s", info["type"])
                 logger.error(info)
         except Exception as e:
             console.print_exception()
